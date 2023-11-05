81b823d68b0e588f9ccce5c30c89592898a7ba62
OlafSwiac <olaf.swiac@gmail.com>
2023-11-05 18:08:28 +0100

    Small changes in problem_1 file -> made the problem as a funtion called in main (also fix matplotlib problem, which occurs sometimes for 3.10 python)

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from Task2 import get_functions_root, print_task_2
-
+from problem_1 import get_distribution_plots
 
 def f(x):
     return (x + 1 + x ** 2) ** (1 / 3) - x
 
-
 result = get_functions_root(f, x0=1, alpha=0, eps=0.00000001, maxiter=100)
-
 print_task_2(result)
+
+N = [5, 25, 100, 1000]
+get_distribution_plots(N)
